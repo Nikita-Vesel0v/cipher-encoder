7943d5c43d0594059bb005a1ef8aea7e5643265b
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-08 23:25:47 +0300

    add converting each of char in the input string to binary string and printing it

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,28 @@
 package chucknorris
 
 class CipherEncoder {
-    private var inputData = charArrayOf()
+    private var inputData = mutableListOf<Char>()
+    private var binaryData = mutableMapOf<Char, String>()
     init {
         println("Input string:")
-        inputData = readln().toCharArray()
+        inputData = readln().toCharArray().toMutableList()
     }
-    fun printInputData() {
-        println(inputData.joinToString(" "))
+
+    fun binary() {
+        inputData.forEach {
+            val binary = Integer.toBinaryString(it.code)
+            binaryData[it] = String.format("%07d", binary.toInt())
+        }
+    }
+    fun printBinary() {
+        println("\nThe result:")
+        inputData.forEach {
+            println("$it = ${binaryData[it]}")
+        }
     }
 }
 fun main() {
     val cipherEncoder = CipherEncoder()
-    cipherEncoder.printInputData()
+    cipherEncoder.binary()
+    cipherEncoder.printBinary()
 }
